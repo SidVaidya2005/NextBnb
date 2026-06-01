a8860396aabd49097fe698b770b60d7a8a7633d8
SidVaidya2005 <siddarthvaidya2005@gmail.com>
2026-06-01 07:48:09 +0530

    style(backend): format controllers with Prettier

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -1,25 +1,25 @@
-const asyncHandler = require('../utils/asyncHandler');
+const asyncHandler = require("../utils/asyncHandler");
 
 // TODO: wire to reviewService.
 
 const index = asyncHandler(async (_req, res) => {
-  res.status(501).json({ error: 'Not implemented' });
+  res.status(501).json({ error: "Not implemented" });
 });
 
 const show = asyncHandler(async (_req, res) => {
-  res.status(501).json({ error: 'Not implemented' });
+  res.status(501).json({ error: "Not implemented" });
 });
 
 const create = asyncHandler(async (_req, res) => {
-  res.status(501).json({ error: 'Not implemented' });
+  res.status(501).json({ error: "Not implemented" });
 });
 
 const update = asyncHandler(async (_req, res) => {
-  res.status(501).json({ error: 'Not implemented' });
+  res.status(501).json({ error: "Not implemented" });
 });
 
 const remove = asyncHandler(async (_req, res) => {
-  res.status(501).json({ error: 'Not implemented' });
+  res.status(501).json({ error: "Not implemented" });
 });
 
 module.exports = { index, show, create, update, remove };
