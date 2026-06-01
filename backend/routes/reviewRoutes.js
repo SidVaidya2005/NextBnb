46606650933b4935f8cb142db76c58ce18da5a5e
SidVaidya2005 <siddarthvaidya2005@gmail.com>
2026-06-01 07:48:09 +0530

    style(backend): format routers with Prettier

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const reviewController = require('../controllers/reviewController');
-const { requireAuth } = require('../middleware/auth');
+const express = require("express");
+const reviewController = require("../controllers/reviewController");
+const { requireAuth } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get('/', reviewController.index);
-router.post('/', requireAuth, reviewController.create);
-router.get('/:id', reviewController.show);
-router.put('/:id', requireAuth, reviewController.update);
-router.delete('/:id', requireAuth, reviewController.remove);
+router.get("/", reviewController.index);
+router.post("/", requireAuth, reviewController.create);
+router.get("/:id", reviewController.show);
+router.put("/:id", requireAuth, reviewController.update);
+router.delete("/:id", requireAuth, reviewController.remove);
 
 module.exports = router;
--- backend/routes/wishlistRoutes.js
+++ backend/routes/wishlistRoutes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const wishlistController = require('../controllers/wishlistController');
-const { requireAuth } = require('../middleware/auth');
+const express = require("express");
+const wishlistController = require("../controllers/wishlistController");
+const { requireAuth } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get('/', requireAuth, wishlistController.index);
-router.post('/:listingId', requireAuth, wishlistController.add);
-router.delete('/:listingId', requireAuth, wishlistController.remove);
+router.get("/", requireAuth, wishlistController.index);
+router.post("/:listingId", requireAuth, wishlistController.add);
+router.delete("/:listingId", requireAuth, wishlistController.remove);
 
 module.exports = router;
